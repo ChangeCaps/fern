dc7dfbff8bb6f9d8dd959173237bf97c246666c4
Hjalte Nannestad <hjalte.nannestad@protonmail.com>
2022-07-18 12:47:04 +0200

    update Program struct

--- src/ir/program.rs
+++ src/ir/program.rs
@@ -1,10 +1,16 @@
-use crate::compiler::{FunctionSignatures, Types};
+use std::collections::HashMap;
 
-use super::Blocks;
+use crate::{
+    compiler::{FunctionSignatures, Types},
+    id::FunctionId,
+};
+
+use super::{Blocks, Function};
 
 #[derive(Clone, Debug, Default)]
 pub struct Program {
     pub types: Types,
     pub signatures: FunctionSignatures,
     pub blocks: Blocks,
+    pub functions: HashMap<FunctionId, Function>,
 }
